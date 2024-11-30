d3e30740ee8a4298f81d5d9b48ed17d8e5884f7a
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-11-30 09:09:52 +0530

    Update 2024-11-30

---
+++ scenario.sh
@@ -0,0 +1,7 @@
+# You accidentally committed sensitive credentials to
+# your local repository but haven't pushed yet.
+git filter-branch --force --index-filter \
+    'git rm --cached --ignore-unmatch path/to/sensitive_file' \
+    --prune-empty --tag-name-filter cat -- --all
+
+How would you remove this commit while keeping the changes in your working directory?
